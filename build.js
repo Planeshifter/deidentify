0c154f32cafe01a3a7a8b4dee71e0e1c719fc08b
Philipp Burckhardt <pburckhardt@outlook.com>
2015-09-13 19:11:08 -0400

    [UPDATE] build process

--- build.js
+++ build.js
@@ -4,7 +4,8 @@
 
 // MODULES //
 
-var Download = require( 'download' ),
+var childProcess = require( 'child_process' ),
+	Download = require( 'download' ),
 	fs = require( 'fs' ),
 	path = require( 'path' );
 
@@ -16,13 +17,34 @@ var dirPath = path.join( './app', 'stanford-ner-2014-10-26' );
 
 // DOWNLOAD STANFORD NER //
 
-var stats = fs.statSync( dirPath );
-if ( !stats.isDirectory() ) {
-	console.log( 'Downloading Stanford NER tool' );
-	new Download( { extract: 'true' } )
-		.get( 'http://nlp.stanford.edu/software/stanford-ner-2014-10-26.zip' )
-		.dest( 'build' )
-		.run();
-} else {
-	console.log( 'Stanford NER already downloaded.' );
+fs.stat( dirPath, function( err, stat ) {
+	if ( err || !stat.isDirectory() ) {
+		console.log( '1. Downloading Stanford NER tool' );
+		new Download( { extract: 'true' } )
+			.get( 'http://nlp.stanford.edu/software/stanford-ner-2014-10-26.zip' )
+			.dest( 'app' )
+			.run( buildExecutables );
+	} else {
+		console.log( '1. Done. Stanford NER already downloaded.' );
+		buildExecutables();
+	}
+});
+
+function buildExecutables() {
+	console.log( '2. Create Builds.' );
+	var dirName = __dirname,
+		cmd = 'sh ' + path.join( dirName, 'script/nwjs-shell-builder/nwjs-build.sh' ); 
+		cmd += ' --name="deidentify"';
+		cmd += ' --src="' + path.join( dirName, 'app' ) + '"';
+		cmd += ' --win-icon="' + path.join( dirName, 'resources/app.ico' ) + '"';
+		cmd += ' --target="3" --output-dir="' + path.join( dirName, 'build' ) + '" --build';
+	
+	console.log( cmd )
+	childProcess.exec( cmd, {}, function( err, stdout ) {
+		if ( err ) {
+			throw new Error( 'build::Build processes finished with errors. Value: `' + err + '`' );
+		} else {
+			console.log( '2. Done. Builds successfully created')
+		}
+	});
 }
