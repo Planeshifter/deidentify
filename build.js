3fdb784180b550741320bb8f8bb434b86b3f23f1
Planeshifter <pburckhardt@outlook.com>
2016-11-21 01:21:09 -0500

    Update code style

--- build.js
+++ build.js
@@ -4,16 +4,16 @@
 
 // MODULES //
 
-var childProcess = require( 'child_process' ),
-	Download = require( 'download' ),
-	fs = require( 'fs' ),
-	path = require( 'path' ),
-	spawn = childProcess.spawn;
+var childProcess = require( 'child_process' );
+var Download = require( 'download' );
+var path = require( 'path' );
+var fs = require( 'fs' );
 
 
 // VARIABLES //
 
 var dirPath = path.join( './app', 'stanford-ner-2014-10-26' );
+var spawn = childProcess.spawn;
 
 
 // DOWNLOAD STANFORD NER //
@@ -33,35 +33,32 @@ fs.stat( dirPath, function( err, stat ) {
 
 function buildExecutables() {
 	console.log( '2. Create Builds.' );
-	var dirName = __dirname,
-		cmd = path.join( dirName, 'script/nwjs-shell-builder/nwjs-build.sh' ),
-		args = [
-			'--name=deidentify',
-			'--src=' + path.join( dirName, 'app' ),
-			'--win-icon=' + path.join( dirName, 'app/app.ico' ),
-			'--target=0 1 2 3 4 5',
-			'--output-dir=' + path.join( dirName, 'dist' ),
-			'--build'
-		];
+	var dirName = __dirname;
+	var cmd = path.join( dirName, 'script/nwjs-shell-builder/nwjs-build.sh' );
+	var args = [
+		'--name=deidentify',
+		'--src=' + path.join( dirName, 'app' ),
+		'--win-icon=' + path.join( dirName, 'app/app.ico' ),
+		'--target=0 1 2 3 4 5',
+		'--output-dir=' + path.join( dirName, 'dist' ),
+		'--build'
+	];
 
 	var build = spawn( cmd, args );
-	build.stdout.on('data', function (data) {
-		console.log('stdout: ' + data);
+	build.stdout.on( 'data', function onStdOut( data ) {
+		console.log( 'stdout: ' + data );
 	});
-	build.stderr.on('data', function (data) {
-		console.log('stderr: ' + data);
+	build.stderr.on( 'data', function onStdErr( data ) {
+		console.log( 'stderr: ' + data );
 	});
-	build.on('close', function (code) {
-		console.log('child process exited with code ' + code);
+	build.on( 'close', function onClose( code ) {
+		console.log( 'child process exited with code ' + code );
 		createInstallers();
 	});
-
 }
 
 /**
-* FUNCTION createInstallers()
-*	Generates installers for Windows, Linus and MacOS.
-*
+* Generates installers for Windows, Linus and MacOS.
 */
 function createInstallers() {
 	console.log( '3. Create installers' );
@@ -70,13 +67,13 @@ function createInstallers() {
 	var installers = spawn( cmd, args, {
 		cwd: path.join( __dirname, 'script/nwjs-shell-builder' )
 	});
-	installers.stdout.on( 'data', function( data ) {
+	installers.stdout.on( 'data', function onStdOut( data ) {
 		console.log( 'stdout: ' + data );
 	});
-	installers.stderr.on( 'data', function( data ) {
+	installers.stderr.on( 'data', function onStdErr( data ) {
 		console.log( 'stderr: ' + data);
 	});
-	installers.on( 'close', function( code ) {
+	installers.on( 'close', function onClose( code ) {
 		console.log( 'Child process exited with code ' + code );
 	});
 } // end FUNCTION createInstallers()
